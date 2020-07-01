587d4120d6fcc042add11259889920834c24a290
Mateus Fugita <mateushiwao@gmail.com>
2020-07-01 18:25:55 -0300

    create page Admin

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Admin from './pages/Admin';
 
 export default function Routes(){
     return(
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Home}></Route>
                 <Route path='/login' component={Login}></Route>
+                <Route path='/admin' component={Admin}></Route>
             </Switch>
         </BrowserRouter>
     )
